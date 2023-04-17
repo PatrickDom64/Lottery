416d7d69d954c60266b2c125c977fe8f917e99e1
Patrick Dom <patrickdomsilva@gmail.com>
2023-04-16 21:29:20 -0300

    Refatoração.Testando funcionalidades.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,96 +34,66 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.ButtonGo = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Location = new System.Drawing.Point(6, 30);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(63, 20);
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.UseWaitCursor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(81, 56);
+            this.textBox2.Location = new System.Drawing.Point(80, 30);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(63, 20);
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.UseWaitCursor = true;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(150, 56);
+            this.textBox3.Location = new System.Drawing.Point(154, 29);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(63, 20);
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
             this.textBox3.TabIndex = 2;
+            this.textBox3.UseWaitCursor = true;
             // 
             // ButtonGo
             // 
-            this.ButtonGo.Location = new System.Drawing.Point(220, 56);
+            this.ButtonGo.Location = new System.Drawing.Point(228, 27);
             this.ButtonGo.Name = "ButtonGo";
-            this.ButtonGo.Size = new System.Drawing.Size(85, 23);
+            this.ButtonGo.Size = new System.Drawing.Size(90, 23);
             this.ButtonGo.TabIndex = 3;
             this.ButtonGo.Text = "Go";
             this.ButtonGo.UseVisualStyleBackColor = true;
+            this.ButtonGo.UseWaitCursor = true;
+            this.ButtonGo.Click += new System.EventHandler(this.ButtonGo_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(4, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 122);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(150, 152);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(63, 20);
-            this.textBox4.TabIndex = 8;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(81, 152);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(63, 20);
-            this.textBox5.TabIndex = 7;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(12, 152);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(63, 20);
-            this.textBox6.TabIndex = 6;
+            this.label1.Text = "Good look";
+            this.label1.UseWaitCursor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(317, 192);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(330, 63);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ButtonGo);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Lottery";
             this.UseWaitCursor = true;
@@ -139,10 +109,6 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button ButtonGo;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,28 @@ namespace Lottery
         {
             InitializeComponent();
         }
-        
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            // Criar uma instância da classe Random
+            Random random = new Random();
+
+            // Gerar um número aleatório entre 1 e 100 (inclusive)
+            int numRandom_1 = random.Next(1, 1000);
+            int numRandom_2 = random.Next(1, 1000);
+            int numRandom_3 = random.Next(1, 1000);
+
+            if (numRandom_1 == int.Parse(textBox1.Text) &&
+                numRandom_2 == int.Parse(textBox2.Text) &&
+                numRandom_3 == int.Parse(textBox3.Text)
+                )
+            {
+                MessageBox.Show("congratulations you are very lucky");
+            }
+            else
+            {
+                MessageBox.Show("Sorry, Try again.");
+            }
+        }
     }
 }
